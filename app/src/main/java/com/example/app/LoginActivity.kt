3a43259fcac79fb912b97650cbf1d52fe71f769f
Mohammed Zaki Khan <mohammedkhanzaki@gmail.com>
2023-08-16 23:12:48 +0530

    basic validation

--- app/src/main/java/com/example/app/LoginActivity.kt
+++ app/src/main/java/com/example/app/LoginActivity.kt
@@ -4,17 +4,52 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 class LoginActivity : AppCompatActivity() {
 
+
+    private lateinit var inputEmail : EditText
+    private lateinit var loginInputPassword : EditText
+    private lateinit var inputphone : EditText
+
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        inputEmail = findViewById(R.id.inputEmail)
+        loginInputPassword = findViewById(R.id.loginInputPassword)
+        inputphone = findViewById(R.id.inputphone)
+
+        val clickme = findViewById<Button>(R.id.btnlogin);
+        clickme.setOnClickListener() {
+            val mail = inputEmail.text.toString().trim()
+            val uid = loginInputPassword.text.toString().trim()
+            val phone = inputphone.text.toString().trim()
+
+            if (mail.isEmpty()) {
+                inputEmail.error= "Email Required"
+                return@setOnClickListener
+            }
+            else if (uid.isEmpty()) {
+                loginInputPassword.error="username is required"
+                return@setOnClickListener
+            }
+            else if (phone.isEmpty()){
+                inputphone.error="Phone number is required"
+                return@setOnClickListener
+            }else {
+                Toast.makeText(this, "validation completed" , Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
         val btn = findViewById<TextView>(R.id.textViewSignUp);
         btn.setOnClickListener (View.OnClickListener{
             // Code here executes on main thread after user presses button
--- app/src/main/java/com/example/app/RegisterActivity.kt
+++ app/src/main/java/com/example/app/RegisterActivity.kt
@@ -4,15 +4,66 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import android.window.SplashScreen
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var inputEmail : EditText
+    private lateinit var loginInputPassword : EditText
+    private lateinit var inputphone : EditText
+    private lateinit var inputPassword : EditText
+    private lateinit var inputConfirmpassword : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState);
 
 
-            setContentView(R.layout.activity_register)
+        inputEmail = findViewById(R.id.inputEmail)
+        loginInputPassword = findViewById(R.id.loginInputPassword)
+        inputphone = findViewById(R.id.inputphone)
+
+        val clickme = findViewById<Button>(R.id.btnRegister);
+        clickme.setOnClickListener() {
+            val mail = inputEmail.text.toString().trim()
+            val uid = loginInputPassword.text.toString().trim()
+            val phone = inputphone.text.toString().trim()
+            val  password = inputPassword.toString().trim()
+            val conpass = inputConfirmpassword.toString().trim()
+
+            if (mail.isEmpty()) {
+                inputEmail.error= "Email Required"
+                return@setOnClickListener
+            }
+            else if (uid.isEmpty()) {
+                loginInputPassword.error="Username is required"
+                return@setOnClickListener
+            }
+            else if (phone.isEmpty()){
+                inputphone.error="Phone number is required"
+                return@setOnClickListener
+
+            }
+
+            else if (password.isEmpty()) {
+                inputPassword.error="password is required"
+                return@setOnClickListener
+            }
+            else if (conpass.isEmpty()) {
+                inputPassword.error="confirm password is required"
+                return@setOnClickListener
+            }
+            else {
+                Toast.makeText(this, "validation completed" , Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        setContentView(R.layout.activity_register)
 
         val btn1 = findViewById<TextView>(R.id.alreadyHaveAccount);
         btn1.setOnClickListener (View.OnClickListener{
